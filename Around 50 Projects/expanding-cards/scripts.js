5277831c3a7ff35d1184142e47970c6649937ad0
stanev raven <sstanev@vtechnology.eu>
2024-02-18 00:03:55 +0200

    finished progress bar

--- Around 50 Projects/expanding-cards/scripts.js
+++ Around 50 Projects/expanding-cards/scripts.js
@@ -1,14 +1,20 @@
 const panels = document.querySelectorAll('.panel');
 
+// just to show the array of div is web console
 console.log(panels);
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+
+        //call the function removeActiveClass
         removeActiveClass()
+
+        // adding class=active to the panel we can use the css to extend the content
         panel.classList.add('active')
     })
 })
 
+//create function that remove the class=active
 function removeActiveClass() {
     panels.forEach(panel => {
         panel.classList.remove('active')
